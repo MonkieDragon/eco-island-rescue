29eae8e4f39ac8e422280b0ac63dd0bcfbcaf9e9
MonkieDragon <clivejeffries@hotmail.com>
2025-06-23 22:27:02 +0100

    reloaded html bridge file

--- html5game/tph_gm_html_bridge.js
+++ html5game/tph_gm_html_bridge.js
@@ -1,5 +1,45 @@
 function notifyGameExit() {
   if (window.ReactNativeWebView?.postMessage) {
-    window.ReactNativeWebView.postMessage("game_exit");
+    window.ReactNativeWebView.postMessage(
+      JSON.stringify({
+        type: "game_exit",
+      })
+    );
+  }
+}
+
+function notifyGameSave(data) {
+  if (window.ReactNativeWebView?.postMessage) {
+    window.ReactNativeWebView.postMessage(
+      JSON.stringify({
+        type: "game_save",
+        payload: data,
+      })
+    );
+  }
+}
+
+function notifyGameLoad() {
+  if (window.ReactNativeWebView?.postMessage) {
+    window.ReactNativeWebView.postMessage(
+      JSON.stringify({
+        type: "game_load",
+      })
+    );
+  }
+}
+
+function gameLoad(jsonString) {
+  try {
+    const data = JSON.parse(jsonString);
+
+    // Send data into a bound GML script
+    if (window.gml_Script_restoreGameData) {
+      gml_Script_restoreGameData(data);
+    } else {
+      console.warn("restoreGameData GML script not found.");
+    }
+  } catch (e) {
+    console.error("Error in loadGame:", e);
   }
 }
